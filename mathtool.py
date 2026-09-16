17733693c83f113888bdfcf52f56313e06c3b227
CHAii11 <lemon4k342@gmail.com>
2026-09-16 19:08:49 +0300

    заново

--- mathtool.py
+++ mathtool.py
@@ -1,7 +1,6 @@
 import sys
 import math
 MAX_VALUE = 10000
-
 text_help = (
         "mathtool — решение уравнений вида A*x^2 + B*x + C = 0\n"
         "Использование:\n"
@@ -11,18 +10,14 @@ text_help = (
         "    python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами\n"
         "Коэффициенты A, B, C — целые числа, по модулю не превышающие 10000."
     )
-print(text_help)
-user_input = input("Введите команду и числа: ")
-parts = user_input.split()
+args = sys.argv[1:] 
 
-if user_input == "--help":
+
+if len(args) == 0 or args[0] == '--help': #вывод справки во всех возможных случаях
     print(text_help)
+    sys.exit(0)
 
-if len(parts) == 1 and user_input == "solve":
-   A = int(input("Введите А:"))
-   B = int(input("Введите B:"))
-   C = int(input("Введите C:"))
+if args[0] != 'solve': #если ввели не то что нужно 
+    print('Ошибка: неизвестная команда', file=sys.stderr)
+    sys.exit(1)
 
-if user_input != "solve" and user_input != "--help":
-    print("Ошибка, неизвестная команда")
-    
